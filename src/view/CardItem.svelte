0d8787ee89f0afbfc2ab1acfbb5fa4009333b6d1
Kenan <kenan@local>
2026-04-26 00:19:25 +0800

    build(release): prepare 0.1.1 metadata
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- src/view/CardItem.svelte
+++ src/view/CardItem.svelte
@@ -73,11 +73,11 @@
 
   function escapeHtml(value: string): string {
     return value
-      .replaceAll("&", "&amp;")
-      .replaceAll("<", "&lt;")
-      .replaceAll(">", "&gt;")
-      .replaceAll('"', "&quot;")
-      .replaceAll("'", "&#39;");
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#39;");
   }
 
   function escapeRegExp(value: string): string {
